8d02d627cbb7d428a969a9594c332c0d56e63470
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-11 22:24:36 -0300

    entregando as rotas de acordo com a permissão

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -3,14 +3,27 @@ import { BrowserRouter } from 'react-router-dom';
 import { useAuth } from "../hooks/auth";
 
 import { AdminRoutes } from './admin.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch(user.role) {
+      case 'admin':
+        return <AdminRoutes/>
+      case 'customer':
+        return <CustomerRoutes/>
+      case 'sale':
+        return <SaleRoutes/>
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }

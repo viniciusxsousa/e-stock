686772a02debb4fc840cb676eec94031fcc5d794
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-01-12 20:28:47 -0300

    ajustes para utilizar cookies

--- api/src/server.js
+++ api/src/server.js
@@ -3,12 +3,16 @@ require("express-async-errors");
 const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173/"]
+}));
 
 app.use(routes);
 
